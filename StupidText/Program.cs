aaab103e93138decb63ef2ce63e4ff73117f1176
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-12-31 16:06:08 -0500

    initial commit

---
+++ StupidText/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Input the text you would like to stupidify... ");
+
+string? input = Console.ReadLine();
+
+string result = String.Empty;
+bool isUpper = false;
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (isUpper)
+    {
+        result = result + input[i].ToString().ToUpper();
+    }
+    else
+    {
+        result = result + input[i].ToString().ToLower();
+    }
+    
+    isUpper = !isUpper;
+}
+
+Console.WriteLine(result);
+Console.ReadLine();
